843f90436efa0a8dea1c6b252412b501b9edb502
YoussefKhafaga <ykhfaga@gmail.com>
2021-11-23 13:32:25 +0200

    diagonal check done

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-board = ["1110111", "1111000", "1100000", "1100000", "1000000", "1000000"]
+board = ["0000002", "0000020", "0010200", "0002000", "0020100", "0200100"]
 
 
 def getscore(board):
@@ -8,20 +8,20 @@ def getscore(board):
     print(minscore)
     return maxscore, minscore
 
+
 def getmaxscore(board):
     score = 0
     score += checkcolumn(board, "1")
+    score += checkdiagonal(board, "1")
     for i in range(0, 6):
         score += checkrow(board[i], "1")
-        # indices = getpos(board[i], "1")
-        # for j in indices:
-        #     score += checkdiagonal(i, j, board)
     return score
 
 
 def getminscore(board):
     score = 0
     score += checkcolumn(board, "2")
+    score += checkdiagonal(board, "2")
     for i in range(0, 6):
         score += checkrow(board[i], "2")
     return score
@@ -77,8 +77,43 @@ def checkcolumn(board, value):
         connection = 0
     return score
 
-# def checkdiagonal(index, row):
 
+def checkdiagonal(board, value):
+    score = 0
+    for i in range(0, 6):
+        for j in range(0, 7):
+            score += getdiagonalscore(i, j, board, value)
+            score += getreversediagonalscore(i, j, board, value)
+    return score
+
+def getdiagonalscore(row, column, board, value):
+    score = 0
+    connection = 0
+    while row < 6 and column < 7:
+        if board[row][column] == value:
+            connection += 1
+            if connection == 4:
+                score += 1
+        else:
+            connection = 0
+        row += 1
+        column += 1
+    return score
+
+
+def getreversediagonalscore(row, column, board, value):
+    score = 0
+    connection = 0
+    while row < 6 and column < 7:
+        if board[row][column] == value:
+            connection += 1
+            if connection == 4:
+                score += 1
+        else:
+            connection = 0
+        row += 1
+        column -= 1
+    return score
 
 # play(1, board)
 getscore(board)
